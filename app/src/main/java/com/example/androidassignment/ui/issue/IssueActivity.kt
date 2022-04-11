2f76ee6af72a5b45670bbf5507acaab7588d1290
Anbyeonghun <abh0920one@gmail.com>
2022-04-11 14:36:51 +0900

    [FEAT] intent를 통해 데이터 전달받기

--- app/src/main/java/com/example/androidassignment/ui/issue/IssueActivity.kt
+++ app/src/main/java/com/example/androidassignment/ui/issue/IssueActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidassignment.ui.issue
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.androidassignment.R
 import com.example.androidassignment.base.BaseActivity
 import com.example.androidassignment.databinding.ActivityIssueBinding
@@ -12,5 +14,21 @@ class IssueActivity : BaseActivity<ActivityIssueBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val intent = intent
+
+        if(intent.hasExtra("profile")){
+            binding.profileIv.clipToOutline = true
+            Glide.with(this)
+                .load(Uri.parse(intent.getStringExtra("profile")))
+                .into(binding.profileIv)
+        }
+
+        if(intent.hasExtra("name")){
+            binding.nameTv.text = intent.getStringExtra("name")
+        }
+
+        if(intent.hasExtra("body")){
+            binding.detailTv.text = intent.getStringExtra("body")
+        }
     }
 }
