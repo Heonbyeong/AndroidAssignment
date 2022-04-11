6ea27496731adcf7fc649c5b734e7b2e43e59232
Anbyeonghun <abh0920one@gmail.com>
2022-04-11 10:45:38 +0900

    [CHORE] IssueActivity 추가

--- app/src/main/java/com/example/androidassignment/ui/issue/IssueActivity.kt
+++ app/src/main/java/com/example/androidassignment/ui/issue/IssueActivity.kt
@@ -1,11 +1,12 @@
-package com.example.androidassignment
+package com.example.androidassignment.ui.issue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidassignment.R
 
-class MainActivity : AppCompatActivity() {
+class IssueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_issue)
     }
 }
