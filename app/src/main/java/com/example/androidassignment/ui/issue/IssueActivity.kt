40c05c99202281141c3bf7539e9808f60d511f87
Anbyeonghun <abh0920one@gmail.com>
2022-04-11 11:34:49 +0900

    [FEAT] BaseActivity implements

--- app/src/main/java/com/example/androidassignment/ui/issue/IssueActivity.kt
+++ app/src/main/java/com/example/androidassignment/ui/issue/IssueActivity.kt
@@ -3,10 +3,14 @@ package com.example.androidassignment.ui.issue
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidassignment.R
+import com.example.androidassignment.base.BaseActivity
+import com.example.androidassignment.databinding.ActivityIssueBinding
 
-class IssueActivity : AppCompatActivity() {
+class IssueActivity : BaseActivity<ActivityIssueBinding>(
+    { ActivityIssueBinding.inflate(it)}
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_issue)
+
     }
 }
