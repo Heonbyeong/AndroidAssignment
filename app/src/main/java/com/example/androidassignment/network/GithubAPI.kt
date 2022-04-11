6b64bdab251aaa7e5a6c3144821ee3d3daddaffc
Anbyeonghun <abh0920one@gmail.com>
2022-04-11 11:56:56 +0900

    [FEAT] Github API interface 이슈 검색 method 추가

--- app/src/main/java/com/example/androidassignment/network/GithubAPI.kt
+++ app/src/main/java/com/example/androidassignment/network/GithubAPI.kt
@@ -1,4 +1,14 @@
 package com.example.androidassignment.network
 
+import com.example.androidassignment.data.remote.GithubResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface GithubAPI {
+    @GET("{org}/{repo}/issues")
+    suspend fun getIssueList(
+        @Path("org") org : String,
+        @Path("repo") repo : String
+    ) : Response<GithubResponse>
 }
